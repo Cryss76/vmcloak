517911758a8f14741f278c4021c4039887f2bfaf
Cryss76 <>
2023-03-15 16:38:12 +0100

    I created an abstract interface, to unify the use of platform modules and make sure that any technical details are done in the respective module.

---
+++ vmcloak/remote_platforms/remote_platform_interface.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+
+class remote_platform_interface(ABC):
+    """Provides a generic interface for using remote Platforms."""
+
+    @abstractmethod
+    def clone(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def delimg(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def delvm(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def init(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def install(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def modify(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def snapshot(self):
+        raise NotImplemented
+
