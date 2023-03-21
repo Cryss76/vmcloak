c00bf9e60038a5bdaca77ab7ec23a97b92b352e5
Cryss <s6ankabi@uni-bonn.de>
2023-03-21 15:14:38 +0100

    added placeholder for proxmox remote platform

---
+++ vmcloak/remote_platforms/proxmox.py
@@ -0,0 +1,27 @@
+from vmcloak.remote_platforms.remote_platform_interface import Remote_platform_interface
+
+name = "Proxmox"
+
+class Proxmox(Remote_platform_interface):
+
+    def clone(self):
+        pass
+
+    def delimg(self):
+        pass
+
+    def delvm(self):
+        pass
+
+    def init(self):
+        pass
+
+    def install(self):
+        pass
+
+    def modify(self):
+        pass
+
+    def snapshot(self):
+        pass
+
